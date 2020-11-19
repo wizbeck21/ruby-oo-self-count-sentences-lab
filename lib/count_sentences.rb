597a73273a858c4c510968d2ea4b7a04f2b59baf
Wesley <wes.beck21@outlook.com>
2020-11-18 19:31:49 -0800

    commit tree a9385d8d04069f1e395c1b0a117f24e9beb7ab90

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+   self.split(/\.|\!|\?/).delete_if {|word| word.length < 2}.length
   end
 end
+
+#using regexp you can split the string by multiple punctuations. 
+#/\p/ the backslash and symbol p, indicate it will split wherever 
+#there is a p(symbol) and a space
